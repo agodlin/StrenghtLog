9d4db27238d986c66b4a055c4ea56a9af5ab7786
Godlin <arkady.godlin@intel.com>
2018-01-05 18:54:00 +1000

    Clean weight code

--- app/src/main/java/com/example/agodlin/strengthlog/ui/weight/dummy/BodyWeightContent.java
+++ app/src/main/java/com/example/agodlin/strengthlog/ui/weight/dummy/BodyWeightContent.java
@@ -23,33 +23,6 @@ public class BodyWeightContent {
      */
     public static final Map<String, BodyWeightItem> ITEM_MAP = new HashMap<String, BodyWeightItem>();
 
-    private static final int COUNT = 25;
-
-    static {
-        // Add some sample items.
-        for (int i = 1; i <= COUNT; i++) {
-            addItem(createDummyItem(i));
-        }
-    }
-
-    private static void addItem(BodyWeightItem item) {
-        ITEMS.add(item);
-        ITEM_MAP.put(item.id, item);
-    }
-
-    private static BodyWeightItem createDummyItem(int position) {
-        return new BodyWeightItem(String.valueOf(position), "Item " + position, makeDetails(position));
-    }
-
-    private static String makeDetails(int position) {
-        StringBuilder builder = new StringBuilder();
-        builder.append("Details about Item: ").append(position);
-        for (int i = 0; i < position; i++) {
-            builder.append("\nMore details information here.");
-        }
-        return builder.toString();
-    }
-
     /**
      * A dummy item representing a piece of content.
      */
