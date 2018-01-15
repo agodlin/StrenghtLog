836261515fa3dc19bfb70273524168e2d622c564
Godlin <arkady.godlin@intel.com>
2018-01-15 19:59:54 +0200

    Change card button to add to listview

--- app/src/main/java/com/example/agodlin/strengthlog/ui/exercise/ExerciseRecyclerViewAdapter.java
+++ app/src/main/java/com/example/agodlin/strengthlog/ui/exercise/ExerciseRecyclerViewAdapter.java
@@ -1,6 +1,8 @@
 package com.example.agodlin.strengthlog.ui.exercise;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.media.Image;
 import android.nfc.Tag;
 import android.support.v7.widget.CardView;
@@ -10,15 +12,20 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.agodlin.strengthlog.R;
+import com.example.agodlin.strengthlog.common.Date;
+import com.example.agodlin.strengthlog.common.Exercise;
+import com.example.agodlin.strengthlog.common.Set;
 import com.example.agodlin.strengthlog.db.DataManager;
 import com.example.agodlin.strengthlog.ui.exercise.ExerciseFragment.OnListFragmentInteractionListener;
 import com.example.agodlin.strengthlog.ui.exercise.dummy.DummyContent;
 import com.example.agodlin.strengthlog.ui.exercise.dummy.DummyContent.DummyItem;
+import com.example.agodlin.strengthlog.ui.exercise_name.ExerciseNameContent;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -81,6 +88,30 @@ public class ExerciseRecyclerViewAdapter extends RecyclerView.Adapter<ExerciseRe
             @Override
             public void onClick(View v) {
                 Log.i(TAG, "ImageButton pressed ");
+                AlertDialog.Builder builder = new AlertDialog.Builder(holder.context);
+                final EditText input = new EditText(holder.context);
+                builder.setView(input);
+                builder.setTitle(R.string.insert_exercise_name)
+                        .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                String name = input.getText().toString();
+                                if (name.isEmpty())
+                                {
+                                    Log.d(TAG, "Text Empty, do nothing");
+                                    return;
+                                }
+                                Log.d(TAG, "Text set To : " + name);
+                                Exercise exercise = new Exercise(name, new Date(1,1,1), new ArrayList<Set>());
+                                DataManager.ITEM_MAP.get(holder.header.getText()).add(exercise);
+                                holder.recyclerView.getAdapter().notifyItemInserted(DataManager.ITEM_MAP.get(holder.header.getText()).size()-1);
+                            }
+                        })
+                        .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        });
+                builder.show();
             }
         });
     }
@@ -95,6 +126,7 @@ public class ExerciseRecyclerViewAdapter extends RecyclerView.Adapter<ExerciseRe
         public final TextView header;
         public final TextView footer;
         public final ImageButton imageButton;
+        public final Context context;
         public ViewHolder(View view, Context context) {
             super(view);
             recyclerView = (RecyclerView)itemView.findViewById(R.id.list);
@@ -102,6 +134,7 @@ public class ExerciseRecyclerViewAdapter extends RecyclerView.Adapter<ExerciseRe
             footer = (TextView)itemView.findViewById(R.id.card_footer);
             imageButton = (ImageButton) itemView.findViewById(R.id.card_menu);
             recyclerView.setLayoutManager(new LinearLayoutManager(context));
+            this.context = context;
         }
 
         @Override
